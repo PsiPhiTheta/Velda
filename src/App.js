3bb4cebb61a7f8e93e77edeb4be7c668ffb1783d
Thomas Hollis <thomashollis1@gmail.com>
2023-07-29 18:14:20 +0100

    feat/ UI tweak on click

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import './App.css';
 import mainLogo from './assets/ticket-transparent.png';
 import button from './assets/download-white-icon-button.png';
+import domtoimage from 'dom-to-image';
 
 function App() {
+  const ticketRef = useRef();
+  const [isButtonVisible, setIsButtonVisible] = useState(true);
   const [fetchedData, setFetchedData] = useState();
   const [fadeIn, setFadeIn] = useState(false); // State to control the fade-in effect
 
@@ -27,40 +30,58 @@ function App() {
     return;
   }
 
+  const exportAsImage = async (element, imageFileName) => {
+    setIsButtonVisible(false);
+    domtoimage.toPng(element)
+      .then(function (dataUrl) {
+        var link = document.createElement('a');
+        link.download = `${imageFileName}.png`;
+        link.href = dataUrl;
+        link.click();
+        setIsButtonVisible(true);
+      })
+      .catch(function (error) {
+        console.error('oops, something went wrong!', error);
+      });
+
+  };
+
   const firstClientDate = new Date(fetchedData.clients[0].timestamp);
-  const updatedVisitorCount = fetchedData.visitorCount - 658
+  const updatedVisitorCount = fetchedData.visitorCount - 700
 
   return (
     <div className="App">
-      <header className="App-header">   
-
-      <div className={`main-content ${fadeIn ? 'fade-in' : ''}`}> {/* Apply the fade-in class when fadeIn is true */}      
-        <div className="background-image">
-          <img src={mainLogo} alt="ticket" />
+      <div
+        ref={ticketRef}
+        className={`ticket ${fadeIn ? 'fade-in' : ''}`}
+        style={{
+          height: '722px',
+          width: '343px',
+          backgroundImage: `url(${mainLogo})`,
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat'
+        }}>
+        <div className="text-overlay-1">
+          GODCASTING <br /> S2
         </div>
 
-        <div className="overlay-content">
-          <div className="text-overlay-1">
-            GODCASTING <br/> S2
+        {isButtonVisible ? (
+          <div className="download-button">
+            <img onClick={() => exportAsImage(ticketRef.current, "ticket")} src={button} alt="download" />
           </div>
+        ) : (
+          <div className='button-dummy' />
+        )}
 
-          <a href={mainLogo} download="mainLogo.png">
-            <div className="download-button">
-              <img src={button} alt="download" />
-            </div>
-          </a>
+        <div className="text-overlay-2">
+          {`${firstClientDate.toLocaleDateString()} ${firstClientDate.toLocaleTimeString()}`}
+        </div>
 
-          <div className="text-overlay-2">
-              {`${firstClientDate.toLocaleDateString()} ${firstClientDate.toLocaleTimeString()}`}
-          </div>
-
-          <div className="text-overlay-3">
-              {`#${String(updatedVisitorCount).padStart(3, '0')}`}
-          </div>
+        <div className="text-overlay-3">
+          {`#${String(updatedVisitorCount).padStart(3, '0')}`}
         </div>
       </div>
-      
-      </header>
     </div>
   );
 }
